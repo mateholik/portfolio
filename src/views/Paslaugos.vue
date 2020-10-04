bed93a5ab4b65ba2269b19e2c19e000c5d7d0f12
mateholik <vaisov.vladimir@gmail.com>
2020-10-04 19:51:01 +0300

    bbz

--- src/views/Paslaugos.vue
+++ src/views/Paslaugos.vue
@@ -1,28 +1,50 @@
 <template>
-    <section>
-        <div class="paslaugos">
-            <h1 class="paslaugos__title"><img :src="require('./../assets/img/icons/www.svg')" />Mano svetainės</h1>
-            <h2 class="paslaugos__subtitle"><img :src="require('./../assets/img/icons/check.svg')" />Reaktyvios (responsive) – pritaikytos telefonams, planšetėms, kompiuteriams. </h2>
-            <h2 class="paslaugos__subtitle"><img :src="require('./../assets/img/icons/check.svg')" />Pritaikytos dažniausiai naudojamoms naršyklėms – Chrome, Firefox, Safari, Edge, Opera, IE. </h2>
-            <h2 class="paslaugos__subtitle"><img :src="require('./../assets/img/icons/check.svg')" />Greitos – optimizuotas kodas, paveiksliukai, skriptai, stiliai. </h2>
-            <h2 class="paslaugos__subtitle"><img :src="require('./../assets/img/icons/check.svg')" />Lengvai valdomos – per WordPress turinio valdymo sistemą lengvai keisite/ trinsite/ pridėsite informaciją/ papildomus turinio puslapius/ produktus. </h2>
-            <br>
-            <h1 class="paslaugos__title"><img :src="require('./../assets/img/icons/services.svg')" />Kitos paslaugos</h1>
-            <h2 class="paslaugos__subtitle"><img :src="require('./../assets/img/icons/check.svg')" />Dizainų karpymas – PSD, sketch, Figma -> HTML, Pixel perfect.</h2>
-            <h2 class="paslaugos__subtitle"><img :src="require('./../assets/img/icons/check.svg')" />WordPress, WooCommerce temų ir pluginų rašymas/ įrašymas.</h2>
-            <h2 class="paslaugos__subtitle"><img :src="require('./../assets/img/icons/check.svg')" />Landeriai, formos, validacijos, checkoutai.</h2>
-            <h2 class="paslaugos__subtitle"><img :src="require('./../assets/img/icons/check.svg')" />Dirbu su HTML, CSS, SASS/BEM, Bootstrap, Tailwind, JS, jQuery, VUE, NUXT, PHP, Twig, GULP, Webpack, GIT, Docker.</h2>
-        </div>
-    </section>
+  <section>
+    <div class="paslaugos">
+      <h1 class="paslaugos__title">
+        <img :src="require('./../assets/img/icons/www.svg')" />Mano svetainės
+      </h1>
+      <h2 class="paslaugos__subtitle">
+        <img :src="require('./../assets/img/icons/check.svg')" />Reaktyvios (responsive) – pritaikytos telefonams, planšetėms, kompiuteriams.
+      </h2>
+      <h2 class="paslaugos__subtitle">
+        <img :src="require('./../assets/img/icons/check.svg')" />Pritaikytos dažniausiai naudojamoms naršyklėms – Chrome, Firefox, Safari, Edge, Opera, IE.
+      </h2>
+      <h2 class="paslaugos__subtitle">
+        <img :src="require('./../assets/img/icons/check.svg')" />Greitos – optimizuotas kodas, paveiksliukai, skriptai, stiliai.
+      </h2>
+      <h2 class="paslaugos__subtitle">
+        <img :src="require('./../assets/img/icons/check.svg')" />Lengvai valdomos – per WordPress turinio valdymo sistemą lengvai keisite/ trinsite/ pridėsite informaciją/ papildomus turinio puslapius/ produktus.
+      </h2>
+      <br />
+      <h1 class="paslaugos__title">
+        <img :src="require('./../assets/img/icons/services.svg')" />Kitos paslaugos
+      </h1>
+      <h2 class="paslaugos__subtitle">
+        <img :src="require('./../assets/img/icons/check.svg')" />Dizainų karpymas – PSD, sketch, Figma -> HTML, Pixel perfect.
+      </h2>
+      <h2 class="paslaugos__subtitle">
+        <img :src="require('./../assets/img/icons/check.svg')" />WordPress, WooCommerce temų ir pluginų rašymas/ įrašymas.
+      </h2>
+      <h2 class="paslaugos__subtitle">
+        <img :src="require('./../assets/img/icons/check.svg')" />Landeriai, formos, validacijos, checkoutai.
+      </h2>
+      <h2 class="paslaugos__subtitle">
+        <img :src="require('./../assets/img/icons/check.svg')" />Dirbu su HTML, CSS, SASS/BEM, Bootstrap, Tailwind, JS, jQuery, VUE, NUXT, Node.js, Express, PHP, Twig, GULP, Webpack, GIT, Docker.
+      </h2>
+      <h2 class="paslaugos__subtitle">
+        <img :src="require('./../assets/img/icons/check.svg')" />Konsultuoju pradedančiuosius front-end programuotojus html, css ir js klausimais.
+      </h2>
+    </div>
+  </section>
 </template>
 
 <script>
-    export default {
-        name: 'Portfolio',
-    }
+export default {
+  name: "Portfolio",
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
